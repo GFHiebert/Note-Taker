d42087ec0ceba93af98f09810d89dfe7f0d8fca9
GHBash <hiebert.garrett67@gmail.com>
2020-07-01 19:44:10 -0700

    Added all complete files and functionality with server connections

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+const app = require("express").Router();
+
+const connection = require("../db/connection");
+
+app.get("/api/notes", (req,res)=>{
+    connection.query("SELECT * FROM notes", (err, response)=>{
+        return res.json(response);
+    });
+});
+
+app.post("/api/notes", (req,res)=>{
+    connection.query("INSERT INTO notes SET ?", req.body, (err, response)=>{
+        return res.json(response);
+    });
+});
+
+app.delete("/api/notes/:id", (req,res)=>{
+
+    const id = req.params.id;
+    connection.query("DELETE FROM notes WHERE ?", {id:id}, (err, response)=>{
+        return res.json(response);
+    });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static("./public"));
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on port ${PORT}`);
+})
+
